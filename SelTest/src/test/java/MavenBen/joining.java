8dcbca9b6df8e3745edc549d86d079584334144c
Bency23 <benzy_leo@yahoo.com>
2021-10-15 20:11:16 +0530

    15evening

--- SelTest/src/test/java/MavenBen/joining.java
+++ SelTest/src/test/java/MavenBen/joining.java
@@ -1,19 +1,34 @@
 package MavenBen;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
 public class joining {
 	
+	@Test
 	private void syso() {
-		// TODO Auto-generated method stub
-
 		
-		System.out.println("fgsjgsf");
-		System.out.println("hbhej");
+
+		System.setProperty("webdriver.gecko.driver","C://Selenium jars//geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver. get("https://www.google.com/");
+		
+		driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("hello world");
+		
 	}
 
 	
 	private void syso1() {
 		// TODO Auto-generated method stub
-System.out.println("I am on 15Oct branch");
-System.out.println(" hello am the second one");
+
+System.out.println("I am on 15Oct second branch");
+
+
 	}
 }
