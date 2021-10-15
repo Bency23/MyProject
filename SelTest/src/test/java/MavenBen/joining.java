5cd4df64417f38aca23ba176c4a6fb8cd5c1ed5b
Bency23 <benzy_leo@yahoo.com>
2021-10-15 15:27:03 +0530

    master push

--- SelTest/src/test/java/MavenBen/joining.java
+++ SelTest/src/test/java/MavenBen/joining.java
@@ -14,5 +14,6 @@ public class joining {
 	private void syso1() {
 		// TODO Auto-generated method stub
 System.out.println("I am on 15Oct second branch");
+System.out.println("am master push");
 	}
 }
